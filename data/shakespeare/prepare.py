f88450b0f1d62fce7ee5230c787c5c4d63c6138d
martinabeleda <martin.abeleda@gmail.com>
2023-01-22 14:46:22 -0800

    Add dataset

---
+++ data/shakespeare/prepare.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+from loguru import logger
+
+DATA_URL = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+DATA_PATH = "input.txt"
+
+
+if not os.path.exists(DATA_PATH):
+    with open(DATA_PATH, "w") as f:
+        f.write(requests.get(DATA_URL).text)
+
+with open(DATA_PATH, "r") as f:
+    data = f.read()
+
+logger.info(f"Length of dataset in characters: {len(data)}")
+
+logger.info(f"Sample text:\n{data[:1000]}")
